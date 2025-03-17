7660b6c845893fa6467a2b80cc40780756483995
gwoo301 <gwoo301@gmail.com>
2025-03-18 08:37:52 +0900

    commit

---
+++ inventory/src/main/java/labshoppubsubpubsub/domain/OrderPlaced.java
@@ -0,0 +1,17 @@
+package labshoppubsubpubsub.domain;
+
+import java.util.*;
+import labshoppubsubpubsub.domain.*;
+import labshoppubsubpubsub.infra.AbstractEvent;
+import lombok.*;
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String customerId;
+    private Double amount;
+}
